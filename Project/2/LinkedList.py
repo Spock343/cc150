026726133a835e7573ab100c8d8999205414b9ab
Spock343 <2228296495@qq.com>
2021-02-02 22:57:12 +0800

    更新至2.5

--- Project/2/LinkedList.py
+++ Project/2/LinkedList.py
@@ -1,11 +1,18 @@
 class Node:
-  data = 0
-  Next = None
-  def __init__(self, data):
-    self.data = data
+    data = 0
+    Next = None
+    def __init__(self, data):
+        self.data = data
     
 def traverseList(Node):
-  while(Node != None):
-    print(Node.data, end = " ")
-    Node = Node.Next
+    while(Node != None):
+        print(Node.data, end = " ")
+        Node = Node.Next
     
+def getLinkedList(List):
+    head = Node(List[0])
+    temp = head
+    for i in range(1, len(List)):
+        temp.Next = Node(List[i])
+        temp = temp.Next
+    return head
